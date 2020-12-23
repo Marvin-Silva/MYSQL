ec8a066946ef9f56c4c0d0697103df217a807e4f
Marvin-Silva <dejesusdasilva.marvin>
2020-12-23 11:24:45 +0100

    ajout Droits utilisateurs

---
+++ DroitDesUtilisateurs.sql
@@ -0,0 +1,5 @@
+GRANT USAGE ON *.* TO `staff`@`localhost`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `breizhvideo`.* TO `staff`@`localhost`;
+
+GRANT USAGE ON *.* TO `client`@`localhost`;
+GRANT SELECT ON `breizhvideo`.* TO `client`@`localhost`;
